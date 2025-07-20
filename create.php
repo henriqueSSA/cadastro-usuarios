8133cad4eedbb98be85cdc4dfc2e984c99aa614c
Henrique Campos <henrique.campos71@gmail.com>
2025-07-20 19:24:22 -0300

    implementes new features screen create

--- create.php
+++ create.php
@@ -4,15 +4,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Novo Cadastro</title>
 </head>
 
 <body>
     <form>
-        <label for="fname">First name:</label><br>
-        <input type="text" id="fname" name="fname"><br>
-        <label for="lname">Last name:</label><br>
-        <input type="text" id="lname" name="lname">
+        <h1>Formulario Multi-Etapas</h1>
+        <form class="style-form" action="/action_page.php" autocomplete="on">
+            <label for="name">Nome:</label><br>
+            <input type="text" id="name" name="name" required><br>
+            <label for="email">Email:</label><br>
+            <input type="email" id="email" name="email" required><br><br>
+            <label for="data">Data De Nascimento:</label>
+            <input type="date" id="data" name="data" required><br>
+            <input type="submit" value="Enviar">
+        </form>
     </form>
 
 </body>
