adadcc5281128411499b99b31f6b4318b23126fe
Henrique Campos <henrique.campos71@gmail.com>
2025-07-20 23:09:13 -0300

    add new features create.php

--- create.php
+++ create.php
@@ -9,18 +9,32 @@
 </head>
 
 <body>
-    <form>
-        <h1>Formulario Multi-Etapas</h1>
-        <form class="style-form" action="/action_page.php" autocomplete="on">
-            <label for="name">Nome:</label><br>
-            <input type="text" id="name" name="name" required><br>
-            <label for="email">Email:</label><br>
-            <input type="email" id="email" name="email" required><br><br>
-            <label for="data">Data De Nascimento:</label>
-            <input type="date" id="data" name="data" required><br>
-            <input type="submit" value="Enviar">
-        </form>
-    </form>
+    <div class="style-tform">
+        <h1>Formulário Multi-Etapas</h1>
+        <div class="style-form">
+            <form action="/action_page.php" autocomplete="on" onsubmit="mostrarLoading()">
+                <label for="name">Nome:</label><br>
+                <input type="text" id="name" name="name" required><br><br>
+                <label for="email">Email:</label><br>
+                <input type="email" id="email" name="email" required><br><br>
+                <label for="data_nasc">Data de Nascimento:</label><br>
+                <input type="date" id="data_nasc" name="data_nasc" max="2009-07-20" required><br><br>
+                <label for="idade">Idade:</label><br>
+                <input type="number" id="idade" name="idade" required><br>
+                <button class="style-btcadastro" type="button" onclick="RedirectForms()">Proximo</button>
+            </form>
+            <div class="barra-container">
+                <div class="barra-progresso" id="barra-progresso"></div>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        function mostrarLoading() {
+            // Mostra o loading
+            document.getElementById('barra-progresso').style.display = 'block';
+        }
+    </script>
 
 </body>
 
